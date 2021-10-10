c917dd35be10bb5224f5eef06b48b70f0184af5f
derrick <40855937+Derrickvibe@users.noreply.github.com>
2021-10-10 11:05:36 +0000

    added google form link to endpoint constants

--- src/constants/EndPointConstant.js
+++ src/constants/EndPointConstant.js
@@ -1,5 +1,5 @@
 export const endPoints = {
-  GOOGLE_FORM_URL:"#",
+  GOOGLE_FORM_URL:"https://docs.google.com/forms/d/e/1FAIpQLSdBK3zzaLQ3BjBs8whF07cQ8Cc7yUul0IDSo68B2M5pq3hryA/viewform",
   FACEBOOK_URL: "#",
   INSTAGRAM_URL: "#",
   TWITTER_URL: "#"
