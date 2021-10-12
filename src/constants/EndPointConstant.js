e86e8cff4cdc0329c4747e8e47b58aa0f3168935
derrick <40855937+Derrickvibe@users.noreply.github.com>
2021-10-12 18:45:32 +0000

    added social media links

--- src/constants/EndPointConstant.js
+++ src/constants/EndPointConstant.js
@@ -1,6 +1,6 @@
 export const endPoints = {
   GOOGLE_FORM_URL:"https://docs.google.com/forms/d/e/1FAIpQLSdBK3zzaLQ3BjBs8whF07cQ8Cc7yUul0IDSo68B2M5pq3hryA/viewform",
-  FACEBOOK_URL: "#",
-  INSTAGRAM_URL: "#",
-  TWITTER_URL: "#"
+  FACEBOOK_URL: "https://www.facebook.com/Omsu-2013-107909985005412/",
+  INSTAGRAM_URL: "https://www.instagram.com/omsu__2013/",
+  TWITTER_URL: "https://twitter.com/Omsu__2013"
 } 
