c855d59dd78a6f1ec84dc6ff6b4ffd07f4195142
derrick <40855937+Derrickvibe@users.noreply.github.com>
2021-10-17 13:04:37 +0000

    updating who join images

--- src/components/WhoJoin.js
+++ src/components/WhoJoin.js
@@ -6,7 +6,7 @@ function WhoJoin(props) {
     <div className="lg:px-14">
        <h3 className="capitalize text-center text-4xl lg:text-6xl font-extrabold ">See who joined</h3>
         <p className="text-center break-words pt-3 font-bold text-lg text-gray-600">
-          Over <span className="text-green"> 100 + Mawulians</span> have joined the OMSU 13 network. <br/> It will be nice to see you too , tap on the plus sign to join us now!
+          Over <span className="text-green"> 100 + Mawulians</span> have joined the OMSU 13 network. <br/> It will be nice to see you too!
         </p>
 
         <div className="container mx-auto flex justify-center flex-wrap pt-10 px-14 ">
--- src/data/People.js
+++ src/data/People.js
@@ -1,45 +1,56 @@
-import CardImage  from '../assets/oldstudent.png'
+import CardImage from '../assets/oldstudent.png'
+import CardImage1  from '../assets/mcdennis.png'
+import CardImage2  from '../assets/elijah.png'
+import CardImage3  from '../assets/cyril.png'
+import CardImage4  from '../assets/derrick.png'
+import CardImage5  from '../assets/walas.png'
+import CardImage6  from '../assets/tsifodze.png'
+import CardImage7  from '../assets/ebilly.png'
+import CardImage8  from '../assets/mcactonio.png'
+import CardImage9  from '../assets/tukor.png'
+import CardImage10  from '../assets/loyalty.png'
+
 
 export const people = [
   {
     id:1,
-    image:CardImage
+    image:CardImage1
   },
   {
     id:2,
-    image:CardImage
+    image:CardImage2
   },
   {
     id:3,
-    image:CardImage
+    image:CardImage3
   },
   {
     id:4,
-    image:CardImage
+    image:CardImage4
   },
   {
     id:5,
-    image:CardImage
+    image:CardImage5
   },
   {
     id:6,
-    image:CardImage
+    image:CardImage6
   },
   {
     id:7,
-    image:CardImage
+    image:CardImage7
   },
   {
     id:8,
-    image:CardImage
+    image:CardImage8
   },
   {
     id:9,
-    image:CardImage
+    image:CardImage9
   },
   {
     id:10,
-    image:CardImage
+    image:CardImage10
   },
   {
     id:11,
