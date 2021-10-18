3b47f18dde6503ebcf26c6ff6f475555586fe335
derrick <40855937+Derrickvibe@users.noreply.github.com>
2021-10-18 12:36:57 +0000

    updated the events data

--- src/data/Event.js
+++ src/data/Event.js
@@ -1,31 +1,35 @@
-import CardImage  from '../assets/cardpic.png'
+import CardImage  from '../assets/pork.png'
+import CardImage2  from '../assets/paint_sip.png'
+import CardImage3  from '../assets/spag.png'
+import CardImage4  from '../assets/xbox-vs-ps5.png'
+
 export const events = [
   {
     id: 1,
-    title: "Creative Works",
-    description: "This is the official platform for old Mawulians from the 2013 batch.",
-    date: "6th May, 2021",
+    title: "Pork & sobolo meetup",
+    description: "Meet with Mawulians in Ho at Klassic Bites over some pork and drinks",
+    date: "Date coming soon",
     image: CardImage // imageUrl
   },
   {
     id: 2,
-    title: "Creative Works",
-    description: "This is the official platform for old Mawulians from the 2013 batch.",
-    date: "6th May, 2021",
-    image: CardImage // imageUrl
+    title: "Paint & sip",
+    description: "Meet, sip some wine and paint any structure in Mawuli school. Life is art lol",
+    date: "Date coming soon",
+    image: CardImage2 // imageUrl
   },
   {
     id: 3,
-    title: "Creative Works",
-    description: "This is the official platform for old Mawulians from the 2013 batch.",
-    date: "6th May, 2021",
-    image: CardImage // imageUrl
+    title: "Spags in a bucket",
+    description: "Remember those days eating spagheti in a bucket? Let's do this again !!! The best team to prepare the food and finish it first.",
+    date: "Date coming soon",
+    image: CardImage3 // imageUrl
   },
   {
     id: 4,
-    title: "Creative Works",
-    description: "This is the official platform for old Mawulians from the 2013 batch.",
-    date: "6th May, 2021",
-    image: CardImage // imageUrl
+    title: "PS5 or Xbox?",
+    description: "Whether PS5 or Xbox? Bring your console and let's rock the game .Don't blame pad when you lose",
+    date: "Date coming soon",
+    image: CardImage4 // imageUrl
   },
 ]
